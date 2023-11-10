e78d5f58344b148b14c79ae598f0c88f9272ce35
aaronfern <aaron.francis.fernandes@sap.com>
2023-11-10 16:50:54 +0530

    Minor changes

--- generator/lib/commands.py
+++ generator/lib/commands.py
@@ -155,7 +155,7 @@ class InfoGenerator:
                 apt_get_commands.extend(InfoGenerator._get_package_name_and_bins(command))
             if isinstance(command, Pip):
                 pip_commands.extend(InfoGenerator._get_package_name_and_bins(command))
-            if isinstance(command, (Curl, Git, Execute)):
+            if isinstance(command, (Curl, Git, Execute, Copy)):
                 command_tools = command.get_tools()
                 for tool in command_tools:
                     if tool.get_info() is not None:
