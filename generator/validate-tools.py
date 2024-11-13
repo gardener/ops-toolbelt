b56bc1d9e02925b49690fb4f0ad344cd122147d3
Dimitar Ivanov <dimitar.ivanov04@sap.com>
2024-11-13 17:32:26 +0200

    Correcting linter errors on the python code
    I've disabled B404, as it's just generated when you call `subprocess`.
    In addition - I've dissabled B603 as we rely on the problematic part
    for the analisys.

--- generator/validate-tools.py
+++ generator/validate-tools.py
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import argparse
-import subprocess
+import subprocess  # nosec B404  # We don't have the exploitable B602 and can't go without subprocess
 import shlex
 import re
 
@@ -20,13 +20,19 @@ def validate_tools(commands_list):
                 cmd_line = re.sub(r"\$\(echo \${TARGETARCH}.*\)", "amd64", cmd_line)
                 cmd_line = re.sub(r"\${TARGETARCH}", "amd64", cmd_line)
                 try:
+                    # Shell is set to false to limit the change for shell injection
+                    # but we need check=true to get the return code
+                    # as this is the actual validation of commands.
+                    # B603 is disabled for that reason
                     subprocess.run(
-                        ["curl", "-sLf", shlex.quote(cmd_line)],
+                        ["/usr/bin/curl", "-sLf", shlex.quote(cmd_line)],
                         stdout=subprocess.DEVNULL,
                         stderr=subprocess.DEVNULL,
-                        check=True)
+                        check=True,
+                        shell=False
+                    )  # nosec B603
                 except subprocess.CalledProcessError as e:
-                    errors.append('Command "{}" failed with exit code {}'.format(e.cmd, e.returncode))
+                    errors.append(f'Command "{e.cmd}" failed with exit code {e.returncode}')
     return errors
 
 
@@ -42,6 +48,7 @@ validation_errors = validate_tools(commands_list)
 if len(validation_errors) == 0:
     print("Validation of tools was successful")
     exit(0)
-else:
-    print("Validation of tools failed with the following errors:\n{}".format('\n'.join(validation_errors)))
-    exit(1)
+
+validation_errors_str: str = "\n".join(validation_errors)
+print(f"Validation of tools failed with the following errors:\n{validation_errors_str}")
+exit(1)
